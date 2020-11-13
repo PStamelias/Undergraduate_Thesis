41b2ba7f8eed0c8a70d335ea033bb5f1712b6653
skagos <prokopisstamelias@hotmail.com>
2020-11-13 19:14:51 +0200

    Jsoup Library Made

--- TikTokCrawling.java
+++ TikTokCrawling.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.util.logging.*;
+import org.jsoup.Jsoup;  
+import org.jsoup.nodes.Document;
 class TikTokCrawling{
     public static void main (String[] args){
-    	
+    	String url="https://www.tiktok.com/foryou?lang=en";
+    	Document doc = Jsoup.parse(url);
     }
 }
--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 
-all:compile
-
+run: 
+	java -cp jsoup-1.13.1.jar:. TikTokCrawling 
 
 compile:
-	javac TikTokCrawling.java
+	javac -cp jsoup-1.13.1.jar TikTokCrawling.java 
 
 clean:
 	rm -r TikTokCrawling.class
