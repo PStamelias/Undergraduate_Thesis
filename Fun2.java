6345ec3e6409f0c3ab2384bd15b6b2755091dac9
skagos <prokopisstamelias@hotmail.com>
2020-11-17 20:14:10 +0200

    Message

--- Fun2.java
+++ Fun2.java
@@ -6,8 +6,11 @@ public  class Fun2{
 	public  String MySQL_Database_Creation(Video[] table,int coun) throws Exception{
     	Connection c = null;
       try {
-         Class.forName("org.postgresql.Driver");
-         c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/prokopis","prokopis","123");
+        Class.forName("org.postgresql.Driver");
+        c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/prokopis","prokopis","123");
+        Statement stmt = c.createStatement();
+        stmt.close();
+        c.close();
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName()+": "+e.getMessage());
