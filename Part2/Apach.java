1236478e19893e916edb91a2d2cc364d532e4584
skagos <prokopisstamelias@hotmail.com>
2021-03-25 15:39:05 +0200

    Last Commit2

--- Part2/Apach.java
+++ Part2/Apach.java
@@ -3,10 +3,14 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriter;
+import java.nio.file.Paths;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -16,10 +20,50 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.store.FSDirectory;
-
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 class Apach{
-	public static void main (String[] args) throws IOException{
-		
+	public static void main (String[] args) throws Exception{
+		IndexWriter writer=writer_creation();
+	}
+	public static IndexWriter writer_creation() throws Exception{
+		String INDEX_DIR="index";
+		FSDirectory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+	    IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+	    IndexWriter writer = new IndexWriter(dir, config);
+		Connection c=null;
+		int num;
+		String name;
+	    String Sound_Tag;
+	    String date;
+		try{
+			Class.forName("org.postgresql.Driver");
+		    c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/prokopis","prokopis","123");
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		Statement stmt1=c.createStatement();
+	    ResultSet rs=stmt1.executeQuery("select * from TikTokVideoDataTable ");
+	    while(rs.next()){
+	      Document doc = new Document();
+	      num = rs.getInt(1);
+	      name= rs.getString(2);
+	      Sound_Tag=rs.getString(4);
+	      date=rs.getString(8);
+	      String y=String.valueOf(num);  
+	      doc.add(new StringField("num",y, Field.Store.YES));
+          doc.add(new StringField("name", name, Field.Store.YES));
+          doc.add(new TextField("Sound_Tag", Sound_Tag, Field.Store.YES));
+          doc.add(new StringField("date", date, Field.Store.YES));
+          writer.addDocument(doc);
+	    }
+	    writer.close();
+	    stmt1.close();
+	    c.close();
+	    return writer;
 	}
 }
